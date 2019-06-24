9b450321e02cb1e2d4ff39253292deba3c650c2b
Apurva Kemkar <apurvakemkar@apurvas-mbp.home>
2019-06-23 20:43:30 -0400

    added custom  domain

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Skills from './components/Skills'
+import Education from './components/Education'
+import Experience from './components/Experience'
+import Work from './components/Work'
+import Contact from './components/Contact'
+import Sidebar from './components/Sidebar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="colorlib-page">
+        <div id="container-wrap">
+          <Sidebar></Sidebar>
+          <div id="colorlib-main">
+            <Home></Home>
+            <About></About>
+            <Services></Services>
+            <Skills></Skills>
+            <Education></Education>
+            <Experience></Experience>
+            <Work></Work>
+            {/*<Blog></Blog>*/}
+            <Contact></Contact>
+          </div>
+      	</div>
+      </div>
   );
 }
 
